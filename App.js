77f6415964c24ebf591608be63e3566b58b13465
ethanGaming12 <168164295+ethanGaming12@users.noreply.github.com>
2024-04-26 11:55:26 +0530

    Add files via upload
    uploaded

---
+++ App.js
@@ -0,0 +1,15 @@
+import User from './components/axios/User'
+import Nav from './components/route/Nav';
+import { Routes, Route } from 'react-router-dom';
+
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+     <h1>React Application</h1>
+     <Nav/>
+    </div>
+  );
+}
+
+export default App;
